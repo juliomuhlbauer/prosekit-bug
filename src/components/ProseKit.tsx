a345724a18ff09723f491d617186f5ae866c1191
Júlio Mühlbauer <juliowernermm@gmail.com>
2025-05-20 09:13:09 -0300

    add toolbar

--- src/components/ProseKit.tsx
+++ src/components/ProseKit.tsx
@@ -1,23 +1,29 @@
 import "prosekit/basic/style.css";
 import "prosekit/basic/typography.css";
 
-import { defineBasicExtension } from "prosekit/basic";
 import { createEditor } from "prosekit/core";
 import { ProseKit } from "prosekit/react";
 import { useMemo } from "react";
+import { Stack } from "@chakra-ui/react";
+import { Toolbar } from "./Toolbar";
+import { defineExtension } from "./extensions";
 
 export function ProseKitEditor() {
   const editor = useMemo(() => {
-    const extension = defineBasicExtension();
+    const extension = defineExtension();
     return createEditor({ extension });
   }, []);
 
   return (
     <ProseKit editor={editor}>
-      <div
-        ref={editor.mount}
-        style={{ outline: "auto", padding: "1rem" }}
-      ></div>
+      <Stack>
+        <Toolbar />
+
+        <div
+          ref={editor.mount}
+          style={{ outline: "auto", padding: "1rem" }}
+        ></div>
+      </Stack>
     </ProseKit>
   );
 }
---
+++ src/components/Toolbar.tsx
@@ -0,0 +1,116 @@
+import { useEditor } from "prosekit/react";
+import { Button, HStack, IconButton } from "@chakra-ui/react";
+import {
+  LuBold,
+  LuHeading1,
+  LuHeading2,
+  LuHeading3,
+  LuItalic,
+  LuRedo,
+  LuUndo,
+} from "react-icons/lu";
+import { EditorExtension } from "./extensions";
+
+function ToolbarButton({
+  active,
+  disabled,
+  onClick,
+  tooltip,
+  children,
+}: {
+  active: boolean;
+  disabled: boolean;
+  onClick: () => void;
+  tooltip: string;
+  children: React.ReactNode;
+}) {
+  return (
+    <button
+      style={{
+        backgroundColor: active ? "#F5F5F5" : "transparent",
+        border: "none",
+        borderRadius: "4px",
+        padding: "4px",
+      }}
+      aria-label={tooltip}
+      disabled={disabled}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+}
+
+export function Toolbar() {
+  const editor = useEditor<EditorExtension>({ update: true });
+
+  return (
+    <HStack px={16} py={4}>
+      <ToolbarButton
+        active={false}
+        disabled={!editor.commands.undo.canExec()}
+        onClick={editor.commands.undo}
+        tooltip="Undo"
+      >
+        <LuUndo />
+      </ToolbarButton>
+
+      <ToolbarButton
+        active={false}
+        disabled={!editor.commands.redo.canExec()}
+        onClick={editor.commands.redo}
+        tooltip="Redo"
+      >
+        <LuRedo />
+      </ToolbarButton>
+      <ToolbarButton
+        active={editor.marks.bold.isActive()}
+        disabled={!editor.commands.toggleBold.canExec()}
+        onClick={editor.commands.toggleBold}
+        tooltip="Bold"
+      >
+        <LuBold />
+      </ToolbarButton>
+
+      {/* <ToolbarButton
+        active={editor.marks.italic.isActive()}
+        disabled={!editor.commands.toggleItalic.canExec()}
+        onClick={editor.commands.toggleItalic}
+        tooltip="Italic"
+      >
+        <LuItalic />
+      </ToolbarButton> */}
+
+      <ToolbarButton
+        active={editor.nodes.heading.isActive({ level: 1 })}
+        disabled={!editor.commands.toggleHeading.canExec({ level: 1 })}
+        onClick={() => editor.commands.toggleHeading({ level: 1 })}
+        tooltip="Heading 1"
+      >
+        <LuHeading1 />
+      </ToolbarButton>
+      <ToolbarButton
+        active={editor.nodes.heading.isActive({ level: 2 })}
+        disabled={!editor.commands.toggleHeading.canExec({ level: 2 })}
+        onClick={() => editor.commands.toggleHeading({ level: 2 })}
+        tooltip="Heading 2"
+      >
+        <LuHeading2 />
+      </ToolbarButton>
+      <ToolbarButton
+        active={editor.nodes.heading.isActive({ level: 3 })}
+        disabled={!editor.commands.toggleHeading.canExec({ level: 3 })}
+        onClick={() => editor.commands.toggleHeading({ level: 3 })}
+        tooltip="Heading 3"
+      >
+        <LuHeading3 />
+      </ToolbarButton>
+      {/* <ImageUploadPopover
+        disabled={!editor.commands.insertImage.canExec()}
+        tooltip="Insert Image"
+      >
+        <div className="i-lucide-image h-5 w-5" />
+      </ImageUploadPopover> */}
+    </HStack>
+  );
+}
